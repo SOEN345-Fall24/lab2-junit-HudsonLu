2b456cc7e12b4c654b45066f7a0869b0e5d9d638
Hudson Lu <131294600+HudsonLu@users.noreply.github.com>
2024-09-13 12:56:55 -0400

    Update ExampleTest.java

--- ExampleTest.java
+++ ExampleTest.java
@@ -6,25 +6,25 @@ public class ExampleTest {
     @Test
     public void testFunctionAPositive() {
         Example example = new Example(4, -8);
-        assertEquals("Calc sum incorrect", 6, example.Function_A(2));
+        assertEquals("Function A - Calc sum incorrect", 6, example.Function_A(2));
     }
 
     @Test
     public void testFunctionANegative() {
         Example example = new Example(4, -8);
-        assertEquals("Calc sum incorrect", -5, example.Function_A(-1));
+        assertEquals("Function A - Calc sum incorrect", -5, example.Function_A(-1));
     }
 
     @Test
     public void testFunctionBLessThanSix() {
         Example example = new Example(4, -8);
-        assertEquals("Calc sum incorrect", -5, example.Function_B(3));
+        assertEquals("Function B - Calc sum incorrect", -5, example.Function_B(3));
     }
 
     @Test
     public void testFunctionBMoreThanSix() {
         Example example = new Example(4, -8);
-        assertEquals("Calc sum incorrect", 15, example.Function_B(7));
+        assertEquals("Function B - Calc sum incorrect", 15, example.Function_B(7));
     }
 
 }
